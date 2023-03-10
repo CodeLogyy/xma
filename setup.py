6eb6021edc78c9e41d9fb2f0784f97060a505bc9
CodeLogy - کدلوژی <127527813+CodeLogyy@users.noreply.github.com>
2023-03-10 19:15:52 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name="Xma",
+    version= '0.1',
+    packages=["Xma"],
+    license='GNU',
+    description="Xma(stream)",
+    keywords=['Xma'],
+    author_email="codelogyofficial@gmail.com",
+    url='https://github.com/MrFploit/Xma',
+    author = 'Codelogy',
+    py_modules=["Xma"],
+    classifiers=[
+    'Development Status :: 3 - Beta',
+    'Intended Audience :: MR.M',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.8',
+  ]
+)
